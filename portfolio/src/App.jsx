b81ea752ae6c1e5ad262b82143cde628f93acd00
Eric <e.kalin12@gmail.com>
2023-04-11 15:37:42 -0400

    added icons and restructured Sass config to seperate sass files by sections for easier readability

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,10 @@
-import { useState } from 'react'
-import './App.scss'
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import beigeBlackIcon from './assets/Icons/color1.png';
+import orangeGreenIcon from './assets/Icons/color2.png';
+import yellowBlueIcon from './assets/Icons/color3.png';
+import purpleIcon from './assets/Icons/color4.png'
+import './assets/sass/App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,11 +17,25 @@ function App() {
           <a href="#TechStack">SKILLS</a>
           <a href="#Portfolio">PORTFOLIO</a>
           <a href="#ContactMe">CONTACT</a>
+          <motion.div className="colorSchemeSelector" whileHover={{ x: '200px' }}>
+            <motion.div className="css-text">COLOR</motion.div>
+            <motion.div className="css-hidden">
+            <img src={yellowBlueIcon}/>
+            <img src={orangeGreenIcon}/>
+            <img src={purpleIcon}/>
+            <img src={beigeBlackIcon}/>
+            </motion.div>
+          </motion.div>
         </div>
-        <h1>Eric Kalin</h1>
+        <h1>ERIC KALIN</h1>
       </section>
       <section id="AboutMe">
-        <h1>Page Two</h1>
+        <div className="aboutMeText">
+          ABOUT ME
+          ABOUT ME
+          ABOUT ME
+          ABOUT ME
+        </div>
       </section>
       <section id="TechStack">
         <h1>Page Three</h1>
