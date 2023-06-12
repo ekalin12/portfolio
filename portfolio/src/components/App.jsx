69125eba76c51bbce695eef183491952d13ddf4f
Eric <e.kalin12@gmail.com>
2023-06-12 19:42:51 -0400

    successfully implemented color scheme changing functionality and also adjusted css to make ContactMe SVG scalable for multiple screen types

--- portfolio/src/components/App.jsx
+++ portfolio/src/components/App.jsx
@@ -1,10 +1,7 @@
 import React, {useState, useRef, useEffect } from 'react';
 import { gsap, Power2 } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import beigeBlackIcon from './assets/Icons/color1.png';
-// import orangeGreenIcon from './assets/Icons/color2.png';
-// import yellowBlueIcon from './assets/Icons/color3.png';
-// import purpleIcon from './assets/Icons/color4.png';
+
 import Homepage from './homepage.jsx';
 import AboutMe from './aboutMe.jsx';
 import Skills from './skills.jsx'
--- portfolio/src/components/homepage.jsx
+++ portfolio/src/components/homepage.jsx
@@ -1,6 +1,11 @@
 import React, {useRef, useEffect} from 'react';
 import { gsap, Power2 } from "gsap";
 
+import beigeBlackIcon from '../assets/Icons/color1.png';
+import orangeGreenIcon from '../assets/Icons/color2.png';
+import yellowBlueIcon from '../assets/Icons/color3.png';
+import purpleIcon from '../assets/Icons/color4.png';
+
 function Homepage(props) {
   console.log('PROPSSS: ', props)
 
@@ -11,6 +16,11 @@ function Homepage(props) {
   let contactMe = useRef(null);
 
   useEffect(() => {
+    const primaryOne = getComputedStyle(document.documentElement).getPropertyValue("--app-primaryColorOne")
+    console.log('primary color one: ', primaryOne)
+  }, [])
+
+  useEffect(() => {
      props.timeline.to([aboutMe, skills, portfolio, contactMe], {
       y: 20,
       stagger: {
@@ -19,7 +29,19 @@ function Homepage(props) {
       ease: Power2.out,
       duration: 1,
      })
-  })
+  });
+
+  function changeColorScheme(primaryColorOne, primaryColorTwo, secondaryColor, accentColor) {
+
+    // console.log('inputs: ', primaryColorOne, primaryColorTwo, secondaryColor, accentColor);
+    // console.log('style: ', document.documentElement)
+    // const root = document.documentElement;
+    root.style.setProperty("--app-primaryColorOne", primaryColorOne);
+    root.style.setProperty("--app-primaryColorTwo", primaryColorTwo)
+    root.style.setProperty("--app-secondaryColor", secondaryColor)
+    root.style.setProperty("--app-accentColor", accentColor)
+    console.log(root.style.getPropertyValue('--app-primaryColorTwo'))
+  }
 
   return (
     <section id="Homepage">
@@ -28,6 +50,17 @@ function Homepage(props) {
           <a className="am-techstack" href="#Skills" ref={ el => skills = el}>SKILLS</a>
           <a className="am-portfolio" href="#Portfolio" ref={ el => portfolio = el}>PORTFOLIO</a>
           <a className="am-contactme" href="#ContactMe" ref={ el => contactMe = el}>CONTACT</a>
+          <ul style={{listStyleType: "none"}}>
+            <li><a href="#" class="button">
+            <span class="icon">+</span>
+            <span class="text">
+              <img src={beigeBlackIcon} onClick={() => {changeColorScheme('#E3DDCB', '#4B4947', '#000000', '#FFFFFF')}}/>
+              <img src={orangeGreenIcon} onClick={() => {changeColorScheme('#33532F', '#FC8E0E', '#000000', '#FFFFFF')}} />
+              <img src={yellowBlueIcon} onClick={() => {changeColorScheme('#D2C904', '#222836', '#000000', '#FFFFFF')}} />
+              <img src={purpleIcon} onClick={() => {changeColorScheme('#B6A3BE', '#67444C', '#000000', '#FFFFFF')}} />
+            </span>
+          </a></li>
+          </ul>
         </div>
         <h1>Eric Kalin</h1>
         <div className="marquee">
