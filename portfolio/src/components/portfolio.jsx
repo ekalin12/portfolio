dbea1ba461a35726b0401b05928fa8f937263646
Eric <e.kalin12@gmail.com>
2023-06-27 16:17:10 -0400

    successfully restructured files to migrate child component html back to parent level

--- portfolio/src/components/portfolio.jsx
+++ portfolio/src/components/portfolio.jsx
@@ -1,110 +1,110 @@
 import React, { useRef, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import MrPort3000 from '../assets/images/Mr.Port3000.png';
-import ApiExtension from '../assets/images/ApiExtension.png';
-import FindYourSpace from '../assets/images/FindYourSpace.png';
-import UnderConstruction from '../assets/images/under-construction.png';
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+// import MrPort3000 from '../assets/images/Mr.Port3000.png';
+// import ApiExtension from '../assets/images/ApiExtension.png';
+// import FindYourSpace from '../assets/images/FindYourSpace.png';
+// import UnderConstruction from '../assets/images/under-construction.png';
 
 
-import { Pagination, Navigation } from "swiper";
+// import { Pagination, Navigation } from "swiper";
 
 
 function Portfolio() {
 
 
-  return (
-  <section id="Portfolio" className="section">
+  // return (
+  // <section id="Portfolio" className="section">
 
-    <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-      >
-        <SwiperSlide className="card swiper-slide">
-        <div class="image-content">
-          <span class="overlay"></span>
-          <div class="card-image">
-              <img src={MrPort3000} alt="" class="card-img" />
-          </div>
-      </div>
-      <div class="card-content">
-          <h2 class="name">Mr.Port 3000</h2>
-          <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
-      </div>
-        </SwiperSlide>
-        <SwiperSlide className="card swiper-slide">
-        <div class="image-content">
-          <span class="overlay"></span>
-          <div class="card-image">
-              <img src={ApiExtension} alt="" class="card-img" />
-          </div>
-      </div>
-      <div class="card-content">
-          <h2 class="name">API Extension</h2>
-          <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
-      </div>
-        </SwiperSlide>
-        <SwiperSlide className="card swiper-slide">
-        <div class="image-content">
-          <span class="overlay"></span>
-          <div class="card-image">
-              <img src={FindYourSpace} alt="" class="card-img" />
-          </div>
-      </div>
-      <div class="card-content">
-          <h2 class="name">Find Your Space</h2>
-          <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
-      </div>
-        </SwiperSlide>
-        <SwiperSlide className="card swiper-slide">
-        <div class="image-content">
-          <span class="overlay"></span>
-          <div class="card-image">
-              <img src={UnderConstruction} alt="" class="card-img" />
-          </div>
-      </div>
-      <div class="card-content">
-          <h2 class="name">Work in progress...</h2>
-          <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
-      </div>
-        </SwiperSlide>
-        <SwiperSlide className="card swiper-slide">
-        <div class="image-content">
-          <span class="overlay"></span>
-          <div class="card-image">
-              <img src={UnderConstruction} alt="" class="card-img" />
-          </div>
-      </div>
-      <div class="card-content">
-          <h2 class="name">Work in progress...</h2>
-          <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
-      </div>
-        </SwiperSlide>
-        <SwiperSlide className="card swiper-slide">
-        <div class="image-content">
-          <span class="overlay"></span>
-          <div class="card-image">
-              <img src={UnderConstruction} alt="" class="card-img" />
-          </div>
-      </div>
-      <div class="card-content">
-          <h2 class="name">Work in progress...</h2>
-          <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
-      </div>
-        </SwiperSlide>
-      </Swiper>
+  //   <Swiper
+  //       slidesPerView={3}
+  //       spaceBetween={30}
+  //       loop={true}
+  //       pagination={{
+  //         clickable: true,
+  //       }}
+  //       navigation={true}
+  //       modules={[Pagination, Navigation]}
+  //     >
+  //       <SwiperSlide className="card swiper-slide">
+  //       <div class="image-content">
+  //         <span class="overlay"></span>
+  //         <div class="card-image">
+  //             <img src={MrPort3000} alt="" class="card-img" />
+  //         </div>
+  //     </div>
+  //     <div class="card-content">
+  //         <h2 class="name">Mr.Port 3000</h2>
+  //         <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+  //     </div>
+  //       </SwiperSlide>
+  //       <SwiperSlide className="card swiper-slide">
+  //       <div class="image-content">
+  //         <span class="overlay"></span>
+  //         <div class="card-image">
+  //             <img src={ApiExtension} alt="" class="card-img" />
+  //         </div>
+  //     </div>
+  //     <div class="card-content">
+  //         <h2 class="name">API Extension</h2>
+  //         <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+  //     </div>
+  //       </SwiperSlide>
+  //       <SwiperSlide className="card swiper-slide">
+  //       <div class="image-content">
+  //         <span class="overlay"></span>
+  //         <div class="card-image">
+  //             <img src={FindYourSpace} alt="" class="card-img" />
+  //         </div>
+  //     </div>
+  //     <div class="card-content">
+  //         <h2 class="name">Find Your Space</h2>
+  //         <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+  //     </div>
+  //       </SwiperSlide>
+  //       <SwiperSlide className="card swiper-slide">
+  //       <div class="image-content">
+  //         <span class="overlay"></span>
+  //         <div class="card-image">
+  //             <img src={UnderConstruction} alt="" class="card-img" />
+  //         </div>
+  //     </div>
+  //     <div class="card-content">
+  //         <h2 class="name">Work in progress...</h2>
+  //         <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+  //     </div>
+  //       </SwiperSlide>
+  //       <SwiperSlide className="card swiper-slide">
+  //       <div class="image-content">
+  //         <span class="overlay"></span>
+  //         <div class="card-image">
+  //             <img src={UnderConstruction} alt="" class="card-img" />
+  //         </div>
+  //     </div>
+  //     <div class="card-content">
+  //         <h2 class="name">Work in progress...</h2>
+  //         <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+  //     </div>
+  //       </SwiperSlide>
+  //       <SwiperSlide className="card swiper-slide">
+  //       <div class="image-content">
+  //         <span class="overlay"></span>
+  //         <div class="card-image">
+  //             <img src={UnderConstruction} alt="" class="card-img" />
+  //         </div>
+  //     </div>
+  //     <div class="card-content">
+  //         <h2 class="name">Work in progress...</h2>
+  //         <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+  //     </div>
+  //       </SwiperSlide>
+  //     </Swiper>
 
-  </section>
-  )
+  // </section>
+  // )
 
 }
 
